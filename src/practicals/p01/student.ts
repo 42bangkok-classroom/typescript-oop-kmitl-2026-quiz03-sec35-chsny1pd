9d73364231aaeb5396553137f96bb4ded9860758
PD <168636679+chsny1pd@users.noreply.github.com>
2026-02-16 09:11:32 +0700

    01

--- src/practicals/p01/student.ts
+++ src/practicals/p01/student.ts
@@ -1,1 +1,8 @@
-export class Student {}
+export class Student {
+    firstname : string = 'John'
+    lastname : string = 'Doe'
+
+    printName(){
+        console.log (this.firstname , this.lastname)
+    }
+}
