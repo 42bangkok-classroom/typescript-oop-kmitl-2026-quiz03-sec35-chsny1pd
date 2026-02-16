3e91d0e0797260b3a539ef2bb18561e31859726d
PD <168636679+chsny1pd@users.noreply.github.com>
2026-02-16 10:04:49 +0700

    08

--- src/practicals/p04/user.ts
+++ src/practicals/p04/user.ts
@@ -1,1 +1,28 @@
-export class User {}
+export class User {
+    public username : string 
+    private password : string 
+    private LOGIN_ATTEMPTS : number = 0
+
+    constructor(username:string,password:string){
+        this.username = username
+        this.password = password
+    }
+
+    public login(password:string) : boolean {
+        if(this.password == password){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+
+    public getLoginAttempts() : number {
+        this.LOGIN_ATTEMPTS += 1
+        return this.LOGIN_ATTEMPTS - 1
+    }
+
+    private validatePassword(){
+        
+    }
+}
