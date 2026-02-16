46e5f579c7ad2f7a6a25a5776519b1dce2b92150
PD <168636679+chsny1pd@users.noreply.github.com>
2026-02-16 09:33:57 +0700

    7

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,18 +1,18 @@
 export class Person {
     firstname : string = 'John'
     lastname : string = 'Doe'
-    private _age : number = 0
+    private age : number = 0
     static COUNTRY : string = 'Thailand'
 
     getFullName(){
         return (`${this.firstname} ${this.lastname}`)
     }
     getAge(){
-        return this._age
+        return this.age
     }
     setAge(age:number){
         if (age >=0) {
-		this._age = age
+		this.age = age
     }
 }
 }
