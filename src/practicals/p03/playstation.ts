695b4def7b01df6f7cd863b3a29e3eb840fb8956
PD <168636679+chsny1pd@users.noreply.github.com>
2026-02-16 10:08:05 +0700

    09

--- src/practicals/p03/playstation.ts
+++ src/practicals/p03/playstation.ts
@@ -1,5 +1,25 @@
 import { Product } from './product'
 
 export class Playstation extends Product {
+   readonly name : string 
+   gen : number 
+   protected price : number = 0
   // TODO: implement class properties, constructor with super(...), and methods
+    constructor(name:string,gen:number,price?:number){
+      super(name)
+      this.name = name
+      this.gen = gen
+      if(price){
+            this.price = price
+        }
+        if(!price){
+            this.price = 0
+        }
+    }
+    getProfile(){
+        return (`${this.name} ${this.price}`)
+    }
+    getDiscountPrice(){
+    this.price = (( 100 - Playstation.DISCOUNT_PERCENT ) * this.price ) / 100
+    }
 }
--- src/practicals/p03/product.ts
+++ src/practicals/p03/product.ts
@@ -1,2 +1,24 @@
-export class Product {}
+export class Product {
+    readonly name : string 
+    protected price : number = 0
+    static readonly DISCOUNT_PERCENT = 10
+
+    constructor(name:string,price?:number){
+        this.name = name
+        if(price){
+            this.price = price
+        }
+        if(!price){
+            this.price = 0
+        }
+    }
+    getPrice(){
+        return this.price
+    }
+    setPrice(price:number){
+        if (price >=0) {
+		this.price = price
+    }
+}
+}
 
